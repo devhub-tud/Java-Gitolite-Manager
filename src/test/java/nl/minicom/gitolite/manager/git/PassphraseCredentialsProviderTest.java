b7219e5fda36c4bf5e4fa647b27192d68678b384
Michael de Jong <michaelj@minicom.nl>
2012-11-16 13:27:57 +0100

    Fixed failing test.

--- src/test/java/nl/minicom/gitolite/manager/git/PassphraseCredentialsProviderTest.java
+++ src/test/java/nl/minicom/gitolite/manager/git/PassphraseCredentialsProviderTest.java
@@ -13,31 +13,31 @@ public class PassphraseCredentialsProviderTest {
 	public void testConstructorWitValidInputs() {
 		new PassphraseCredentialsProvider("");
 	}
-	
-	@Test(expected = NullPointerException.class)
+
+	@Test
 	public void testConstructorWithNullAsPassphrase() {
 		new PassphraseCredentialsProvider(null);
 	}
-	
+
 	@Test
 	public void testThatGetMethodSetsPasswordWhenAsked() {
 		CredentialsProvider provider = new PassphraseCredentialsProvider("passphrase");
-		
+
 		Password password = new Password();
-		
+
 		Assert.assertTrue(provider.get(null, new StringType("", true), password));
 		Assert.assertEquals("passphrase", String.valueOf(password.getValue()));
 	}
-	
+
 	@Test
 	public void testIsInteractiveMethodReturnsFalse() {
 		Assert.assertFalse(new PassphraseCredentialsProvider("").isInteractive());
 	}
-	
+
 	@Test
 	public void testSupportsMethodWhenAskingForPassphrase() {
 		CredentialsProvider provider = new PassphraseCredentialsProvider("passphrase");
 		Assert.assertTrue(provider.supports(new Password()));
 	}
-	
+
 }
