bdb89b51183db77835521f8fb7fad9ce07c40593
Jan-Willem Gmelig Meyling <j.gmeligmeyling@student.tudelft.nl>
2015-04-23 23:22:43 +0200

    Added failing key to the tests

--- src/test/java/nl/minicom/gitolite/manager/models/FailingKeysTest.java
+++ src/test/java/nl/minicom/gitolite/manager/models/FailingKeysTest.java
@@ -16,12 +16,14 @@ import java.util.Collection;
 @RunWith(Parameterized.class)
 public class FailingKeysTest {
 
-    public final static String INVALID_KEY_1 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCftV0wGyhXU2aorR2WmJZCCEbzACRxPpGiI5csVz25uEfQ+QpBsTj43rEDkAQPvO6p+$\n";
-    public final static String INVALID_KEY_2 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCX3E4ePEeuzuJwDB408dkS7gXgfjSWhom03zn2oAz$\n";
-    public final static String INVALID_KEY_3 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5nnzP2T+jLLNg/S6ejEx2+Js0PpgdedZzcNpdwMbVMNIOBrtIkyyxPP+ckQd5Ush8D$\n";
-    public final static String INVALID_KEY_4 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJdlQfcU90mzmVIG4R8QeiqrEI0Kx3KVfM2ux7j/KsN83Pw7BrFvKrlkhfwRjTiQYiwRtVzIVrOlBopel$\n";
-    public final static String INVALID_KEY_5 = "ssh-rsa AAABAAQus1/EbQoJ+3X2gqLdEl+P2o1+Vz3ktV5wtRTAIyWvMIS1UY79RluJM/xylR1b4LroYGXwqCQwX36ep8oPp/taDorawA31Ufe1laC90ji7y213sVVshvgc4Z0kY+8YpAv+1rk8sMD4FG2Cxz3lg8XumLAd4sMNKP4n3B6T4mqrJ/pgNVfRCLYdY24igAbIOOLHLd+TGlPB9yB8fWvlpIcFS1/SENA1hCWuEH/ot/SAypIcEIYcvDia6QcEnSJkQslnr0dFJ62wUFlR2Gqg92XNYe3AsNDuGk0DqwW0AhuMMSGyiBUlUH8xpTi1ttyEAepYNRZsarJGB4GAE22nADUAAACBAPwm9vmk9uO+HxSSNrWPDtiGaqgkr+jQdneDCki/n6Vcr62rbj3sJVhNMPQ4M1NfVX62arSDZby4MjAStAhc/8ORhCizQ+66R025I5yHtpijXcMrMhzEs4n+hfimPSKpxpCdn1wWCMNKW7MWNTnoYd+X9HQB3/ZKdfAf24REz+E7AAAAgQCdHHTe+FEsnZgKPpXRquIGirCUihKAqPPewctEvP/soGEu8Z92fWYy7eEJXOX0btiyP70Ot4DGg2u3tAxfUb7+HMpc/uA8OMEf9QHTiJxxsphdmSHzHDUDaClvrIZi27SgAfIjrLKlKB0rIiJRSkhpLnpRZtOk6KyD662e5wqY5QAAAIBfJTnY39D5Am9VwrSRPp/jGW/5EchqMeTq1AFcGCgt3Q9RhWM3KvCRT+bM3qY8ZJHbZ32epG6V43ueKXADBNb5CXM2PoyJmQlYxVwtwAf2vx54NpmeCkdi8CV9walId8/DMCFI+Pa5NWn5XKIWB+g9Cf41JCMLHoc1AM8g4JHyPA==\n";
-    public final static String INVALID_KEY_6 = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmr/s12PCc3FYDKDhifOnz8qWc0Kb8g42pkor/8UUclIDLjTJqpsrOtSDfI\n";
+    public final static String INVALID_KEY_1 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCftV0wGyhXU2aorR2WmJZCCEbzACRxPpGiI5csVz25uEfQ+QpBsTj43rEDkAQPvO6p+$";
+    public final static String INVALID_KEY_2 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCX3E4ePEeuzuJwDB408dkS7gXgfjSWhom03zn2oAz$";
+    public final static String INVALID_KEY_3 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5nnzP2T+jLLNg/S6ejEx2+Js0PpgdedZzcNpdwMbVMNIOBrtIkyyxPP+ckQd5Ush8D$";
+    public final static String INVALID_KEY_4 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJdlQfcU90mzmVIG4R8QeiqrEI0Kx3KVfM2ux7j/KsN83Pw7BrFvKrlkhfwRjTiQYiwRtVzIVrOlBopel$";
+    public final static String INVALID_KEY_5 = "ssh-rsa AAABAAQus1/EbQoJ+3X2gqLdEl+P2o1+Vz3ktV5wtRTAIyWvMIS1UY79RluJM/xylR1b4LroYGXwqCQwX36ep8oPp/taDorawA31Ufe1laC90ji7y213sVVshvgc4Z0kY+8YpAv+1rk8sMD4FG2Cxz3lg8XumLAd4sMNKP4n3B6T4mqrJ/pgNVfRCLYdY24igAbIOOLHLd+TGlPB9yB8fWvlpIcFS1/SENA1hCWuEH/ot/SAypIcEIYcvDia6QcEnSJkQslnr0dFJ62wUFlR2Gqg92XNYe3AsNDuGk0DqwW0AhuMMSGyiBUlUH8xpTi1ttyEAepYNRZsarJGB4GAE22nADUAAACBAPwm9vmk9uO+HxSSNrWPDtiGaqgkr+jQdneDCki/n6Vcr62rbj3sJVhNMPQ4M1NfVX62arSDZby4MjAStAhc/8ORhCizQ+66R025I5yHtpijXcMrMhzEs4n+hfimPSKpxpCdn1wWCMNKW7MWNTnoYd+X9HQB3/ZKdfAf24REz+E7AAAAgQCdHHTe+FEsnZgKPpXRquIGirCUihKAqPPewctEvP/soGEu8Z92fWYy7eEJXOX0btiyP70Ot4DGg2u3tAxfUb7+HMpc/uA8OMEf9QHTiJxxsphdmSHzHDUDaClvrIZi27SgAfIjrLKlKB0rIiJRSkhpLnpRZtOk6KyD662e5wqY5QAAAIBfJTnY39D5Am9VwrSRPp/jGW/5EchqMeTq1AFcGCgt3Q9RhWM3KvCRT+bM3qY8ZJHbZ32epG6V43ueKXADBNb5CXM2PoyJmQlYxVwtwAf2vx54NpmeCkdi8CV9walId8/DMCFI+Pa5NWn5XKIWB+g9Cf41JCMLHoc1AM8g4JHyPA==";
+    public final static String INVALID_KEY_6 = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmr/s12PCc3FYDKDhifOnz8qWc0Kb8g42pkor/8UUclIDLjTJqpsrOtSDfI";
+    public final static String INVALID_KEY_7 = "ssh-rsa 2048 b2:08:34:36:20:65:a6:64:9c:f5:cc:10:08:68:6d:8f";
+
 
     @Parameters
     public static Collection<Object[]> data() {
@@ -31,7 +33,8 @@ public class FailingKeysTest {
             {INVALID_KEY_3},
             {INVALID_KEY_4},
             {INVALID_KEY_5},
-            {INVALID_KEY_6}
+            {INVALID_KEY_6},
+            {INVALID_KEY_7}
         });
     }
 
